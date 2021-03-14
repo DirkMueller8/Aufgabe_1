df1a9e1038515043891c826d4f3a003e728caa57
Dirk Mueller <dirkmuller8@outlook.de>
2021-03-14 06:42:55 +0100

    Add project files.

---
+++ Aufgabe_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aufgabe_1
+{
+    class Program
+    {
+        // Author: Dirk Mueller
+        // Date: 14.03.2021
+        //
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* This program determines the prime numbers between 1 and 1000         *");
+            Console.WriteLine("************************************************************************");
+        }
+    }
+}
